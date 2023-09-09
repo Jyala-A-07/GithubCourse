5e02651db69114a164d6c813a759ca23c982acd5
Jyala-A-07 <ayushjyala@gmail.com>
2023-09-09 09:48:45 +0530

    added/updated/deleted files

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -1,5 +1,10 @@
 package Pages;
 
 public class LoginPage {
-
+	public static void main() {
+		System.out.println("Hello LP");
+		String un = "admin";
+		String pwd = "admin";
+		System.out.println("Logged In");
+	}
 }
